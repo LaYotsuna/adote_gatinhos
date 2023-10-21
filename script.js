55531c7ecc61ed6240f64dca3c4628028e83cb12
Larissa Cerqueira <larissacerqueirayt@gmail.com>
2023-10-21 14:24:10 -0300

    Adição da função de troca de imagens dos gatinhos

--- script.js
+++ script.js
@@ -1,5 +1,35 @@
 "use strict";
 
-import { someFunction } from "./module.js";
+document.addEventListener("DOMContentLoaded", function () {
+  const selectElement = document.querySelector("#nomes-pets");
+  const images = document.querySelectorAll(".imagens-pets img");
+  const altMap = {
+    frajola: "Odisseu",
+    frida: "Frida",
+    pacoca: "Paçoca",
+    default: "Filhote de gato",
+  };
 
-someFunction("This is a module function");
+  function deactivateImages() {
+    images.forEach((img) => {
+      img.classList.add("desativado");
+      img.classList.remove("ativo");
+    });
+  }
+
+  function activateImage(altText) {
+    const image = document.querySelector(`.imagens-pets img[alt="${altText}"]`);
+    image.classList.add("ativo");
+    image.classList.remove("desativado");
+  }
+
+  selectElement.addEventListener("change", function (e) {
+    const selectedValue = e.target.value;
+
+    deactivateImages();
+
+    // Usar o objeto de mapeamento para ativar a imagem correta ou usar uma imagem padrão.
+    const altText = altMap[selectedValue] || altMap["default"];
+    activateImage(altText);
+  });
+});
